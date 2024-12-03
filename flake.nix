1331f96788faf848c6775afde29d30ea6d7c8968
Junfeng Li <junfengli@meta.com>
2024-12-02 20:34:04 -0800

    nix flake update

--- flake.nix
+++ flake.nix
@@ -2,22 +2,32 @@
   description = "Flake for emacs-im";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
     flake-utils.lib.simpleFlake {
       inherit self nixpkgs;
       name = "emacs-im";
 
-      shell = { pkgs }: pkgs.mkShell {
-        buildInputs = with pkgs; [
-          rustup
-        ] ++ lib.optionals stdenvNoCC.isDarwin [
-          libiconv
-          darwin.apple_sdk.frameworks.Carbon
-        ];
-      };
+      shell =
+        { pkgs }:
+        pkgs.mkShell {
+          buildInputs =
+            with pkgs;
+            [
+              rustup
+            ]
+            ++ lib.optionals stdenvNoCC.isDarwin [
+              libiconv
+              darwin.apple_sdk.frameworks.Carbon
+            ];
+        };
     };
 }
